e1575f3b83e75ba85927d60c65987e26a519b915
PAKAMAS DETSUWAN <6310210267@psu.ac.th>
2023-10-28 21:57:21 +0700

    add update trainer

--- src/main/java/com/example/demo/controller/TrainerController.java
+++ src/main/java/com/example/demo/controller/TrainerController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -118,6 +119,34 @@ public class TrainerController {
 
 			return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
+	}
+	
+	@PutMapping("/trainer/{trainerId}")
+	public ResponseEntity<Object> updatCourse(@PathVariable Integer trainerId, @RequestBody Trainer body) {
+		try {
+			Optional<Trainer> findTrainer = trainerRepository.findById(trainerId);
+
+			if (findTrainer.isPresent()) {
+
+				Trainer trainerEdit = findTrainer.get();
+
+				trainerEdit.setTrainerName(body.getTrainerName());
+				trainerEdit.setTrainertel(body.getTrainertel());
+				trainerRepository.save(trainerEdit);
+
+				return new ResponseEntity<>(trainerEdit,HttpStatus.OK);
+
+			} else {
+
+				return new ResponseEntity<>("Trainer not found",HttpStatus.BAD_REQUEST);
+			}
+			
+		} catch (Exception e) {
+			
+			return new ResponseEntity<>("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR);
+	
+		}
+
 	}
 
 	
